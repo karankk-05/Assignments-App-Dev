d8310a0b5f5115de37ab9c0f83e50ca92ef1d4db
Karan Keer <sushikeer@gmail.com>
2023-12-21 17:55:59 +0530

    Assignments_Done

---
+++ flutter_assg2_ques1/first_app/lib/main.dart
@@ -0,0 +1,340 @@
+
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(4, 249, 225, 1)),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
+  void deleteFavorite() {
+    for (var pair in favorites){
+    favorites.remove(pair);
+    notifyListeners();
+  }
+}
+}
+
+  
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+switch (selectedIndex) {
+  case 0:
+    page = GeneratorPage();
+    break;
+  case 1:
+    page = FavoritesPage();
+    break;
+  case 2:
+    page = ProfilePage();
+    break;
+  default:
+    throw UnimplementedError('no widget for $selectedIndex');
+}
+    return LayoutBuilder(
+      builder: (context,constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.person),
+                      label: Text('Profile'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme=Theme.of(context);
+    var style=theme.textTheme.displayMedium!.copyWith(
+      color:theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color:theme.colorScheme.primary,
+      
+
+        child: Padding(
+         
+          padding: const EdgeInsets.all(8.0),
+          child: Text(pair.asLowerCase,style:style),
+        ),
+      
+    );
+  }
+}
+
+
+
+class FavoritesPage extends StatefulWidget {
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+    
+    IconData delete;
+    delete=Icons.delete;
+    
+    return ListView(
+      children: [
+        
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+            trailing: IconButton(
+                onPressed: () {
+                  setState((){
+                  appState.favorites.remove(pair);
+                });
+                },
+                icon: Icon(delete),
+                
+              ),
+              ),
+          
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+                    onPressed: () {
+                      setState((){appState.favorites.clear();
+                    });
+                    },
+                    child: Text('Delete All')
+                      .animate()
+                      .shake(duration: 700.ms)
+                  ),
+          ),
+      ],
+      
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    
+  return Column(
+    children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(0,50.0,0,0),
+        child: CircleAvatar(
+          backgroundImage:
+          NetworkImage('https://yt3.googleusercontent.com/ytc/APkrFKaD8t4oFlgXcZKoW512Z81CBJuej3K9uHAlSI0x=s900-c-k-c0x00ffffff-no-rj'),
+          radius: 150,
+          
+            ),
+      ),
+
+      Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0,40,0,0),
+            child: Text('Karan Keer',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+          ),
+        ],
+      ),
+      
+       Row(
+         children: [
+           Padding(
+            padding: const EdgeInsets.fromLTRB(22.0,0,0,0),
+            child: Text('karank23@iitk.ac.in',style: TextStyle(fontSize: 12),),
+                 ),
+         ],
+       ),
+
+      Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30,10,0,0),
+            child: Icon(Icons.location_on)
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(2,10,0,0),
+            child: Animate(child: Text('City:Kanpur',style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),)
+            .animate()
+            .fade(duration: 1000.ms)),
+          )
+        ],
+      ),
+
+      Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30,10,0,0),
+            child: Icon(Icons.person)
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(2,10,0,0),
+            
+              child: Animate(child:Text('Roll No: 230531',style: TextStyle(fontSize: 13,fontWeight: FontWeight.bold),)
+              .animate()
+              .fade(duration: 1000.ms)
+            
+            ),
+          )
+        ],
+      ),
+    
+      
+    
+      ]
+    );
+
+    }
+  }
