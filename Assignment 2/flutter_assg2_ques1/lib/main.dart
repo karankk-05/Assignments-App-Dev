4106fe83defb0eb1c2a36e428cbc611e065bea75
Karan Keer <sushikeer@gmail.com>
2023-12-30 20:59:02 +0530

    Assignment 3 done

--- Assignment 2/flutter_assg2_ques1/lib/main.dart
+++ Assignment 2/flutter_assg2_ques1/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Color.fromRGBO(0, 249, 0, 1)),
         ),
-        home: MyHomePage(),
+        home: GeneratorPage(),
       ),
     );
   }
@@ -57,74 +57,7 @@ class MyAppState extends ChangeNotifier {
 
   
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-
-var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    Widget page;
-switch (selectedIndex) {
-  case 0:
-    page = GeneratorPage();
-    break;
-  case 1:
-    page = FavoritesPage();
-    break;
-  case 2:
-    page = ProfilePage();
-    break;
-  default:
-    throw UnimplementedError('no widget for $selectedIndex');
-}
-    return LayoutBuilder(
-      builder: (context,constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.person),
-                      label: Text('Profile'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
-          ),
-        );
-      }
-    );
-  }
-}
 
 
 class GeneratorPage extends StatelessWidget {
